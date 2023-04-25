fc64d90f8030619b5721aa150665a8999871d3e8
dilanurbayraktar <122991745+dilanurbayraktar@users.noreply.github.com>
2023-04-25 18:13:09 -0400

    Update watched.php

--- final/watched.php
+++ final/watched.php
@@ -353,6 +353,30 @@
             <p id="genres"></p>
             
         </div>
+	    
+	    <button onclick="scroll_top()" id="btn" title="Go to top">Top</button>
+
+<script>
+// Get the button
+let mybutton = document.getElementById("btn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function scroll_top() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+</script>
 
     </body>
 </html>
