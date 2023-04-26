1382597d2dc6bffe829e744bdf7a350e2924b3a3
dilanurbayraktar <122991745+dilanurbayraktar@users.noreply.github.com>
2023-04-26 16:31:21 -0400

    Update rec.php

--- final/rec.php
+++ final/rec.php
@@ -295,7 +295,7 @@ curl_close($curl);
             const rec_arr = [];
             console.log("rec arr lenght: " + rec_arr.length);
             let i = 0, page = 1, k = 0, count = 0, count_added = 0, mov_num = 0;
-            while(count_added < 20 && i < rec_arr.length)
+            while(count_added < 20 && i < arr.length)
             {
                 let apiUrl = "https://api.themoviedb.org/3/movie/" + arr[i] + "/recommendations?api_key=fcabeffb7c941589973c5ba5beb7f636&language=en-US&page=" + page;
 
