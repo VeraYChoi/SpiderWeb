49a3ae759a9289b6253cc4c38049defa099e132c
dilanurbayraktar <122991745+dilanurbayraktar@users.noreply.github.com>
2023-04-26 21:22:43 -0400

    Update rec.php

--- final/rec.php
+++ final/rec.php
@@ -344,8 +344,9 @@ curl_close($curl);
                     }
                 }
                 if (count == arr.length) {
-                    document.write("<p class='unavailable'>Please add more movies to your wishlist and favourites to see your personalized movie recommendation list!</p>");
-                    //break;
+                    document.getElementById("show_data").innerHTML = "<p class='unavailable'>Please add more movies to your wishlist and favourites to see your personalized movie recommendation list!</p>";
+                    //document.write("<p class='unavailable'>Please add more movies to your wishlist and favourites to see your personalized movie recommendation list!</p>");
+                    break;
                 }
                 i++;
             }
